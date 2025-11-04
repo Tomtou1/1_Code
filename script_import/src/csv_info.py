1b1e0f1a86b5ae34ea3a93de7fec266b39acb27f
Tomtou1 <leroythomas01@yahoo.fr>
2025-11-04 10:18:46 +0100

    add 3 tests (nbr columns, duplicated, missingdata)

--- script_import/src/csv_info.py
+++ script_import/src/csv_info.py
@@ -3,3 +3,15 @@ import pandas as pd
 def get_nbrline_csv(csv_path):
     df_csv = pd.read_csv(csv_path)
     return len(df_csv)
+
+def get_nbrcolumns_csv(csv_path):
+    df_csv = pd.read_csv(csv_path)
+    return df_csv.shape[1]
+
+def get_doublons_csv(csv_path):
+    df_csv = pd.read_csv(csv_path)
+    return df_csv.duplicated().sum()
+
+def get_missing_data_csv(csv_path):
+    df_csv = pd.read_csv(csv_path)
+    return sum(df_csv.isna().sum())
--- script_import/src/db_info.py
+++ script_import/src/db_info.py
@@ -1,8 +1,17 @@
 from pymongo import MongoClient
+import pandas as pd
 
-def get_nbrline_db(mongoClient,db_name, coll_name):
-
-    db = mongoClient[db_name]
-    collection = db[coll_name]
-
+def get_nbrline_db(collection):
     return collection.count_documents({})
+
+def get_nbrcolumns_db(collection):
+    dfbdd = pd.DataFrame(list(collection.find({}, projection={"_id": 0})))
+    return dfbdd.shape[1]
+
+def get_doublons_db(collection):
+    dfbdd = pd.DataFrame(list(collection.find({}, projection={"_id": 0})))
+    return dfbdd.duplicated().sum()
+
+def get_missing_data_db(collection):
+    dfbdd = pd.DataFrame(list(collection.find({}, projection={"_id": 0})))
+    return sum(dfbdd.isna().sum())
