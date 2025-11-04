db57bfec9535823e8578be67e4b7a4a5a0d4650b
Tomtou1 <leroythomas01@yahoo.fr>
2025-11-04 10:13:02 +0100

    Improvement One test with connection MongoDB

--- script_import/src/db_info.py
+++ script_import/src/db_info.py
@@ -1,13 +1,8 @@
 from pymongo import MongoClient
 
-def get_nbrline_db(coll_name):
+def get_nbrline_db(mongoClient,db_name, coll_name):
 
-    MONGO_HOST = "mongodb"
-    MONGO_PORT = 27017
-    MONGO_DB = "hcare_db"
-    uri = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
-    mongoClient = MongoClient(uri)
-    db = mongoClient[MONGO_DB]
+    db = mongoClient[db_name]
     collection = db[coll_name]
 
     return collection.count_documents({})
--- script_import/tests/test_insert_script.py
+++ script_import/tests/test_insert_script.py
@@ -6,6 +6,13 @@ import db_info
 csv_path = 'data/hcare_dataset_test.csv'
 collection_name = 'traitement' 
 
+MONGO_HOST = "mongodb"
+MONGO_PORT = 27017
+MONGO_DB = "hcare_db"
+MONGO_COLLECTION = "traitement"
+uri = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
+mongoClient = MongoClient(uri)
+
 def test_count_lines():
     
-    assert csv_info.get_nbrline_csv(csv_path) == db_info.get_nbrline_db(collection_name), 'Nbre ligne incorrect: '  + str(csv_info.get_nbrline_csv(csv_path)) + ' dans le csv et ' + str(db_info.get_nbrline_db(bdd_name,collection_name)) + ' dans la bdd'  
+    assert csv_info.get_nbrline_csv(csv_path) == db_info.get_nbrline_db(mongoClient,MONGO_DB,MONGO_COLLECTION), 'Nbre ligne incorrect: '  + str(csv_info.get_nbrline_csv(csv_path)) + ' dans le csv et ' + str(db_info.get_nbrline_db(bdd_name,collection_name)) + ' dans la bdd'  
